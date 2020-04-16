50a4a427fbfcd3629916d6d00d8ce80cef446bfb
Steve Doty <stevedoty16@gmail.com>
2020-04-16 14:37:51 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components'
+
+const UserName = styled.p`
+    width:100%
+`
+const Column = styled.div`
+    float: left;
+    border-radius: 3px;
+    margin: 5px;
+    padding: 5px;
+    width: 45%;
+    background: transparent;
+    color: black;
+    border: 2px solid black;
+`
+const Element = styled.div`
+    border-radius: 3px;
+    margin: 5px;
+    padding: 5px;
+    background: transparent;
+    color: black;
+    border: 2px solid black;
+`
+const ListItem = styled.li`
+    list-style-type: none;
+`
 
 function App() {
+var users = [{'username':'steve'}]
+var title = ['title1']
+var details = ['detail1', 'detail2']
+var subTitles = ['sub1','sub2']
+var dataBase = {
+    title,
+    details,
+    subTitles,
+
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <UserName>{users[0].username}</UserName>
+        <Column>
+            <Element>
+                {title.map(item=>item)} ; {details.map(detail=> detail  )}
+
+                {subTitles.map(sub=><ListItem>sub</ListItem>)}
+            </Element>
+            <Element>
+                {title.map(item=>item)} ; {details.map(detail=> detail  )}
+
+                {subTitles.map(sub=><ListItem>sub</ListItem>)}
+            </Element>
+        </Column>
+        <Column>
+            <Element>
+                {title.map(item=>item)} ; {details.map(detail=> detail  )}
+
+                {subTitles.map(sub=><ListItem>sub</ListItem>)}
+            </Element>
+            <Element>
+                {title.map(item=>item)} ; {details.map(detail=> detail  )}
+
+                {subTitles.map(sub=>sub)}
+            </Element>
+            <Element>
+                {title.map(item=>item)} ; {details.map(detail=> detail  )}
+
+                {subTitles.map(sub=>(<ListItem>sub</ListItem>))}
+            </Element>
+        </Column>
     </div>
   );
 }
