7adddeec4ca4547358b11375ac99dd5171a8f38d
Steve Doty <stevedoty16@gmail.com>
2020-04-16 18:18:03 -0700

    basic layout of elements

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const Column = styled.div`
     margin: 5px;
     padding: 5px;
     width: 45%;
-    background: transparent;
+    background: teal;
     color: black;
     border: 2px solid black;
 `
@@ -20,8 +20,8 @@ const Element = styled.div`
     border-radius: 3px;
     margin: 5px;
     padding: 5px;
-    background: transparent;
-    color: black;
+    background: black;
+    color: white;
     border: 2px solid black;
 `
 const ListItem = styled.li`
@@ -30,45 +30,127 @@ const ListItem = styled.li`
 
 function App() {
 var users = [{'username':'steve'}]
-var title = ['title1']
-var details = ['detail1', 'detail2']
+var title = ['title0','title1','title2','title3','title4','title5','title6','title7','title8','title9',]
+var details = ['detail1', 'detail2','detail3']
 var subTitles = ['sub1','sub2']
 var dataBase = {
     title,
     details,
     subTitles,
-
 }
+ console.log(dataBase)
   return (
+
     <div className="App">
         <UserName>{users[0].username}</UserName>
         <Column>
             <Element>
-                {title.map(item=>item)} ; {details.map(detail=> detail  )}
+                {title[0]}-{details.map(detail=> <>-{detail}</>  )}
 
-                {subTitles.map(sub=><ListItem>sub</ListItem>)}
+                {subTitles.map(sub=>(
+                    <ListItem>{sub}-
+                        {details.map(detail=><>-{detail}</>)}
+                    </ListItem>
+                ))}
             </Element>
             <Element>
-                {title.map(item=>item)} ; {details.map(detail=> detail  )}
+                {title[1]}-{details.map(detail=> <>-{detail}</>  )}
 
-                {subTitles.map(sub=><ListItem>sub</ListItem>)}
+                {subTitles.map(sub=>(
+                    <ListItem>{sub}-
+                        {details.map(detail=><>-{detail}</>)}
+                    </ListItem>
+                ))}
             </Element>
         </Column>
         <Column>
             <Element>
-                {title.map(item=>item)} ; {details.map(detail=> detail  )}
+                {title[2]}-{details.map(detail=> <>-{detail}</>  )}
 
-                {subTitles.map(sub=><ListItem>sub</ListItem>)}
+                {subTitles.map(sub=>(
+                    <ListItem>{sub}-
+                        {details.map(detail=><>-{detail}</>)}
+                    </ListItem>
+                ))}
+            </Element>
+
+
+
+        </Column>
+
+        <Column>
+            <Element>
+                {title[3]}-{details.map(detail=> <>-{detail}</>  )}
+
+                {subTitles.map(sub=>(
+                    <ListItem>{sub}-
+                        {details.map(detail=><>-{detail}</>)}
+                    </ListItem>
+                ))}
             </Element>
             <Element>
-                {title.map(item=>item)} ; {details.map(detail=> detail  )}
+                {title[4]}-{details.map(detail=> <>-{detail}</>  )}
 
-                {subTitles.map(sub=>sub)}
+                {subTitles.map(sub=>(
+                    <ListItem>{sub}-
+                        {details.map(detail=><>-{detail}</>)}
+                    </ListItem>
+                ))}
+            </Element>
+        </Column>
+<Column>
+            <Element>
+                {title[5]}-{details.map(detail=> <>-{detail}</>  )}
+
+                {subTitles.map(sub=>(
+                    <ListItem>{sub}-
+                        {details.map(detail=><>-{detail}</>)}
+                    </ListItem>
+                ))}
             </Element>
             <Element>
-                {title.map(item=>item)} ; {details.map(detail=> detail  )}
+                {title[6]}-{details.map(detail=> <>-{detail}</>  )}
 
-                {subTitles.map(sub=>(<ListItem>sub</ListItem>))}
+                {subTitles.map(sub=>(
+                    <ListItem>{sub}-
+                        {details.map(detail=><>-{detail}</>)}
+                    </ListItem>
+                ))}
+            </Element>
+        </Column>
+        <Column>
+            <Element>
+                {title[7]}-{details.map(detail=> <>-{detail}</>  )}
+
+                {subTitles.map(sub=>(
+                    <ListItem>{sub}-
+                        {details.map(detail=><>-{detail}</>)}
+                    </ListItem>
+                ))}
+            </Element>
+
+
+
+        </Column>
+
+        <Column>
+            <Element>
+                {title[8]}-{details.map(detail=> <>-{detail}</>  )}
+
+                {subTitles.map(sub=>(
+                    <ListItem>{sub}-
+                        {details.map(detail=><>-{detail}</>)}
+                    </ListItem>
+                ))}
+            </Element>
+            <Element>
+                {title[9]}-{details.map(detail=> <>-{detail}</>  )}
+
+                {subTitles.map(sub=>(
+                    <ListItem>{sub}-
+                        {details.map(detail=><>-{detail}</>)}
+                    </ListItem>
+                ))}
             </Element>
         </Column>
     </div>
