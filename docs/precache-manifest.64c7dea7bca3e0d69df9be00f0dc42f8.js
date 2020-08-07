f52691b42e89d4d74f53520f94c60f1bac14ffe6
Steve Doty <stevedoty16@gmail.com>
2020-08-07 07:40:25 -0700

    build docs

---
+++ docs/precache-manifest.64c7dea7bca3e0d69df9be00f0dc42f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6831ee2850773a7720df6fe191be93d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e39983306033e6f01895",
+    "url": "/static/css/main.69252029.chunk.css"
+  },
+  {
+    "revision": "4e38478cf3bcf602f49c",
+    "url": "/static/js/2.3aa601e3.chunk.js"
+  },
+  {
+    "revision": "c607541aec0fb4887a90f60037cb0bdf",
+    "url": "/static/js/2.3aa601e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e39983306033e6f01895",
+    "url": "/static/js/main.585e9a15.chunk.js"
+  },
+  {
+    "revision": "7fb177c0f946bc1d4cce",
+    "url": "/static/js/runtime-main.86a9e4ae.js"
+  }
+]);
